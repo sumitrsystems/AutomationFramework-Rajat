3fc07d0a8883ca5f8dec77bd93a9056faf7b2e30
Rajat <Rajat@SEZLAP-16867.india.rsystems.com>
2024-04-22 15:06:42 +0530

    update paths in dataproviders

--- src/test/java/utility/ExcelDataProvider.java
+++ src/test/java/utility/ExcelDataProvider.java
@@ -21,7 +21,7 @@ public class ExcelDataProvider {
     public Object[][] provideSecondRowData() {
         Object[][] data = new Object[1][2]; // Only one row with two columns (email and password)
         try {
-            Object[][] allData = ExcelUtil.readExcelData("C:\\Users\\Rajat\\IdeaProjects\\untitled\\src\\main\\java\\data\\testdata.xlsx", "Sheet1");
+            Object[][] allData = ExcelUtil.readExcelData("src/main/resources/data/testdata.xlsx", "Sheet1");
             data[0][0] = allData[1][0]; // Second row email
             data[0][1] = allData[1][1]; // Second row password
         } catch (IOException e) {
